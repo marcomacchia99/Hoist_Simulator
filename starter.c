20faedf3171d739624501335d9a47286856a4ec4
Marco <marcomacchia99@gmail.com>
2021-11-17 14:02:50 +0100

    first implementation of starter

--- starter.c
+++ starter.c
@@ -9,11 +9,33 @@
 
 #define SIZE 80
 
-
-int main (int argc, char * argv[])
+int spawn(const char *program, char **arg_list)
 {
-char *fifo_command_motorX = "/tmp/command_motorX";
-
-
+    pid_t child_pid = fork();
+    if (child_pid != 0)
+        return child_pid;
+    else
+    {
+        execvp(program, arg_list);
+        perror("exec failed");
+        return 1;
+    }
 }
 
+int pid_command;
+int pid_inspection;
+int pid_motorX;
+int pid_motorZ;
+int pid_watchdog;
+char buffer[SIZE];
+
+int main(int argc, char *argv[])
+{
+    char *fifo_command_motorX = "/tmp/command_motorX";
+
+  char * arg_list_1[] = { "/usr/bin/hoist",  "-e", "./commandconsole"};
+  char * arg_list_2[] = { "/usr/bin/hoist",  "-e", "./inspectionconsole"}; 
+  char * arg_list_3[] = { "/usr/bin/hoist",  "-e", "./motorX"};
+  char * arg_list_4[] = { "/usr/bin/hoist",  "-e", "./motorZ"};
+  char * arg_list_5[] = { "/usr/bin/hoist",  "-e", "./watchdog"};
+}
